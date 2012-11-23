b31ab5a4bc3382a08a8234bf4b3dd9ab3fb6c215
raz <raziebe@gmail.com>
2012-11-23 23:27:07 +0200

    modified:   user/ec_device.h

--- user/ec_device.h
+++ user/ec_device.h
@@ -1,7 +1,7 @@
 #ifndef __EC_DEVICE_H__
 #define __EC_DEVICE_H__
 
-typedef struct __ec_device__ {
+struct ec_device {
 	int index;
 	struct ether_header mac;
 	char name[16];
@@ -10,6 +10,6 @@ typedef struct __ec_device__ {
 	int sock;
 	int subnet_mask;
 	int link_up;
-} ec_device;
+};
 
 #endif
