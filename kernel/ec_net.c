a7229f94e59b8c485cdcfed1ba1093683c68a065
raz <raziebe@gmail.com>
2012-11-26 23:50:59 +0200

    reset skb once done

--- kernel/ec_net.c
+++ kernel/ec_net.c
@@ -108,7 +108,7 @@ void ec_tx_pkt(	uint8_t *buf __attribute__ ((unused)),
 	struct ec_device* rxdev = txdev->ecat_node->intr[RX_INT_INDEX];
 	ec_device_send(txdev,
 			rxdev->processed_skb);
-	rxdev->processed_skb = 0;
+	txdev->processed_skb = 0;
 }
 
 int ec_is_nic_link_up(e_slave * ecs, struct ec_device *intr)
