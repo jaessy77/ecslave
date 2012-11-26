c4f9c72e55c8e772a54c8cd20601ad6da238bff2
raz <raziebe@gmail.com>
2012-11-26 23:52:24 +0200

    check if packets are currently processed

--- kernel/ecat_protocol.c
+++ kernel/ecat_protocol.c
@@ -12,8 +12,6 @@
 #include <linux/if_ether.h>
 #include <linux/if_arp.h>
 #include <linux/skbuff.h>
-#include <linux/can.h>
-#include <linux/can/core.h>
 #include <linux/ratelimit.h>
 #include <net/net_namespace.h>
 #include <net/sock.h>
@@ -44,21 +42,24 @@ static int returning_pkt(struct sk_buff *skb)
 void ecat_process_pkt(struct sk_buff *skb)
 {
 	int ret;
-	/* 
-	 * first check if packet is traversing back.
-	 * we do not check for for outgoing as it is filtered
-	 * out by the filter driver. 
-	 */
+	
 	ret = returning_pkt(skb);
 	if (ret == 1) {
 		ec_device_send(eslave->intr[RX_INT_INDEX], skb);
+		eslave->intr[RX_INT_INDEX]->processed_skb = NULL;
+		return;
+	}
+	if (eslave->intr[RX_INT_INDEX]->processed_skb){
+		printk("pkt is procssed \n");
+		kfree_skb(skb);
 		return;
 	}
 	if (ret == 0) {
-		struct ec_device *device = eslave->intr[RX_INT_INDEX];
-		device->processed_skb = skb;
+		eslave->intr[RX_INT_INDEX]->processed_skb = skb;
 		ec_process_datagrams(eslave, skb->len, skb->data);
+		return;
 	}
+	printk("should not be here\n");
 }
 
 static int ecat_rcv(struct sk_buff *skb, struct net_device *dev, 
